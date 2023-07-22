1739a026c55f893ad19db4132c38387b96b4fd47
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2023-07-22 20:25:06 +0200

    Add code + readme file

---
+++ README.md
@@ -0,0 +1,75 @@
+# Czujnik wilgotnośći gleby
+[Wersja podstawowa](https://github.com/vBagieta/soil-moisture-sensor/blob/main/basicMoistureSensor.ino)
+
+[Wersja z odczytem temperatury i wilgoci otoczenia (NIEWSPIERANA)](https://github.com/vBagieta/soil-moisture-sensor/blob/main/legacyMoistureSensor.ino)
+
+(Znakiem * została oznaczona wersja dodatkowa, wymagająca dodatkowych czujników)
+## Wymagania
+- [Arduino UNO](https://botland.com.pl/arduino-seria-podstawowa-oryginalne-plytki/1060-arduino-uno-rev3-a000066-7630049200050.html)
+- [Czujnik wilgotności gleby](https://botland.com.pl/gravity-czujniki-pogodowe/10305-dfrobot-gravity-analogowy-czujnik-wilgotnosci-gleby-odporny-na-korozje-sen0193-5903351243155.html?cd=18298825651&ad=&kd=&gclid=CjwKCAjwgqejBhBAEiwAuWHioMwWA4SEuGi1RUKJWuG2f7o5MhYemAxIOgkFh670djLlfkb2HUvX_BoCisoQAvD_BwE)
+- [Wyświetlacz LCD (2x16) wraz z modułem I2C](https://botland.com.pl/wyswietlacze-alfanumeryczne-i-graficzne/2351-wyswietlacz-lcd-2x16-znakow-niebieski-konwerter-i2c-lcm1602-5904422309244.html)
+- [Czujnik wilgotnośći i temperatury](https://botland.com.pl/czujniki-multifunkcyjne/1886-czujnik-temperatury-i-wilgotnosci-dht11-modul-przewody-5903351242448.html?cd=18298825138&ad=&kd=&gclid=CjwKCAjwgqejBhBAEiwAuWHioJbbUovxF6pOOuP1HUgdDNM5c0fvh1O-9PxHxg97GvXvbL3xxiLDmRoC1_MQAvD_BwE)*
+
+## Biblioteki
+- [hd44780](https://www.arduinolibraries.info/libraries/hd44780)
+- [DHT](https://github.com/markruys/arduino-DHT)*
+## Połączenia
+
+### Wyświetlacz
+Pin `GND` z pinem `GND` na Arduino
+
+Pin `VCC` z pinem `5V` na Arduino
+
+Pin `SDA` z pinem `SDA` na Arduino
+
+Pin `SCL` z pinem `SCL` na Arduino
+
+### Czujnik wilgotności gleby
+Pin `GND` z pinem `GND` na Arduino
+
+Pin `VCC` z pinem `3.3V` na Arduino
+
+Pin `AUOT` z pinem `A0` na Arduino
+
+### Czujnik wilgotnośći i temperatury*
+Pin `+` z pinem `5V` na Arduino
+
+Pin `out` z pinem `2` na Arduino
+
+Pin `-` z pinem `GND` na Arduino
+
+
+## Kalibracja czujnika wilgotnośći
+### Kod do kalibracji:
+Pin danych z czujnika został podpięty do pinu `A0`!
+
+```
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int val;
+  val = analogRead(A0);
+  Serial.println(val);
+  delay(100);
+}
+```
+### Kroki
+Na początku zapisz najwyższą wartość, jaką czujnik uzyskuje, gdy jest suchy. Zmień wartość w kodzie (domyślnie 603) na zmierzoną:
+```
+const int dryValue = 603; -> const int dryValue = TWOJA_WARTOŚĆ;
+```
+
+Następnie zmierz wartość, jaką czujnik uzyskuje, gdy jest najbardziej mokry. Zmierz wartość na zmierzoną.
+
+```
+const int wetValue = 603; -> const int wetValue = TWOJA_WARTOŚĆ;
+```
+
+
+Teraz wystarczy załadować kod do Arduino i wszytko powinno działać!
+
+## Propozycje & Poprawki
+Stwórz github [issue](https://github.com/vBagieta/soil-moisture-sensor/issues)
+
+Dla zaawansowanych: [pull request](https://github.com/vBagieta/soil-moisture-sensor/pulls)
---
+++ soil-moisture-sensor.ino
@@ -0,0 +1,419 @@
+//Libraries required to run this code
+#include <Wire.h>
+#include <hd44780.h>                          
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+//Objects
+hd44780_I2Cexp lcd; //Object used to control LCD display over the I2C module
+hd44780_I2Cexp lcd2(0x27); //I2C Adress for the LCD display
+
+//Options
+const int LCD_COLS = 16; //Default LCD parameters
+const int LCD_ROWS = 2;
+
+const int dryValue = 615; //See github for more infromations!
+const int wetValue = 292; //See github for more infromations!
+
+int percent;
+int soilCapacityValue;
+
+String soilHumidityTitle = "Soil Humidity";  //Title on display
+
+//Custom chars
+byte middleEmptyChar[] = {   //There's some errors, but code will work
+  B11111,                    //I'm too lazy to fix that now.
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111
+};
+
+byte rightEmptyChar[] = {
+  B11111,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B11111
+};
+
+byte leftHalfEmptyChar[] = {
+  B11111,
+  B10000,
+  B10100,
+  B10100,
+  B10100,
+  B10100,
+  B10000,
+  B11111
+};
+
+byte middleHalfEmptyChar[] = {
+  B11111,
+  B00000,
+  B11000,
+  B11000,
+  B11000,
+  B11000,
+  B00000,
+  B11111
+};
+
+byte rightHalfEmptyChar[] = {
+  B11111,
+  B00001,
+  B00101,
+  B00101,
+  B00101,
+  B00101,
+  B00001,
+  B11111
+};
+
+byte leftFullChar[] = {
+  B11111,
+  B10000,
+  B10111,
+  B10111,
+  B10111,
+  B10111,
+  B10000,
+  B11111
+};
+
+byte middleFullChar[] = {
+  B11111,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B00000,
+  B11111
+};
+
+byte rightHalfChar[] = {
+  B11111,
+  B00001,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B00001,
+  B11111
+};
+
+byte rightFullChar[] = {
+  B11111,
+  B00001,
+  B11101,
+  B11101,
+  B11101,
+  B11101,
+  B00001,
+  B11111
+};
+
+byte rightRightHalfEmptyChar[] = {
+  B11111,
+  B00001,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B00001,
+  B11111
+};
+
+
+//Code
+void setup()
+{
+  //LCD initializing
+  int status;
+  status = lcd.begin(LCD_COLS, LCD_ROWS);
+
+  //LCD checking
+  if(status) { 
+  status = -status; 
+
+  hd44780::fatalError(status);
+  }
+
+  //Creating chars
+  lcd.createChar(1, middleEmptyChar);
+  lcd.createChar(2, rightEmptyChar);
+  lcd.createChar(3, leftHalfEmptyChar);
+  lcd.createChar(4, middleHalfEmptyChar);
+  lcd.createChar(5, leftFullChar);
+  lcd.createChar(6, middleFullChar);
+  lcd.createChar(7, rightRightHalfEmptyChar);
+  lcd.createChar(8, rightFullChar);
+}
+
+void loop() {
+  soilCapacityValue = analogRead(A0); //Read the value from sensor (Value from A0 analog port)
+  percent = 100-((soilCapacityValue-(dryValue-wetValue))*100)/wetValue;  //Make percent using data
+                                                                         //See github for more informations
+  lcd.print(soilHumidityTitle); //Set title on LCD display
+  lcd.setCursor(0, 1);
+  displayMoistureBar(percent); //Display bar on LCD based on humidity percent
+  displayFixedPercentage(percent); //Display only 0-100 percents next to the bar
+  delay(100); //Delay this loop for 0.1s
+}
+
+void displayMoistureBar(int progress) { //Display bar on LCD (1 Segment = 10%, 1/2 Segment = 5%)
+  if (progress >= 100) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(8);
+  } else if (progress >= 95) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(7);
+  } else if (progress >= 90) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(2);
+  } else if (progress >= 85) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(2);
+  } else if (progress >= 80) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 75) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 70) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 65) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 60) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 55) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 50) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 45) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 40) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 35) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 30) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 25) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 20) {
+    lcd.write(5);
+    lcd.write(6);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 15) {
+    lcd.write(5);
+    lcd.write(4);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 10) {
+    lcd.write(5);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= 5) {
+    lcd.write(3);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  } else if (progress >= -10) {
+    lcd.write(3);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(1);
+    lcd.write(2);
+  }
+}
+
+void displayFixedPercentage(int percentage) {  //This function will display 100% even if real value is (for example) 103.
+  if (percentage >= 100) {                     //Beacuse it is hard to calibrate corecctly the sensor
+    lcd.setCursor(11, 1);
+    lcd.print("100%   ");
+  } else if (percentage <= 0) {
+    lcd.setCursor(11, 1);
+    lcd.print("0%   ");
+  } else {
+    lcd.setCursor(11, 1);
+    lcd.print(String(percentage) + "%   ");
+  }
+}
+
+//This is the END! Enjoy.
+//Developer's github: https://github.com/vBagieta/soil-moisture-sensor
